31818b8438ecdb6c1a58b4f3e256d951afc20893
Salim <mohammadsalim017427@gmail.com>
2025-12-12 20:22:47 +0600

    u

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next").NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "*",
-      },
-    ],
+    domains: ["*"],
+    remotePatterns: [],
+    unoptimized: true,
   },
 };
 
